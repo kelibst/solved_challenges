1a8ac298937cd23191df03a12bf3f348de680b19
Keli Booster <kbooster17@gmail.com>
2020-10-24 18:53:26 +0000

    add min_stack

--- ruby/linked_list.rb
+++ ruby/linked_list.rb
@@ -2,96 +2,93 @@
 
 class Node
   attr_accessor :value, :next_node
-  
+
   def initialize(value, next_node = nil)
-	  @value = value
+    @value = value
     @next_node = next_node
   end
 end
 
 class LinkedList
-   attr_reader :tail
-   @head = nil
-   @tail = nil
+  attr_reader :tail
+  @head = nil
+  @tail = nil
 
-   def add(num)
-      node = Node.new(num)
+  def add(num)
+    node = Node.new(num)
 
-      if @head.nil?
-        @head = node
-        @tail = node
-      else
-        @tail.next_node = node
-        @tail = node
+    if @head.nil?
+      @head = node
+      @tail = node
+    else
+      @tail.next_node = node
+      @tail = node
+    end
+  end
+
+  def get(ind)
+    cur = @head
+    count = 0
+    value = nil
+
+    while cur
+      if count === ind
+        value = cur.value
+        break
       end
-   end
+      cur = cur.next_node
+      count += 1
+    end
 
-   def get(ind)
-      cur = @head
-      count = 0
-      value = nil
+    value
+  end
 
-      while cur
-        if count === ind
-          value = cur.value
-          break
-        end
-        cur = cur.next_node
-        count +=1
-      end
-      
-      value
-   end
+  def add_at(ind, item)
+    new_node = Node.new(item)
+    if ind == 0
+      new_node.next_node = @head
+      @head = new_node
+      @tail = @head.next_node if @tail == @head
+    else
+      prev_node = earlier_node(ind)
+      cur = prev_node.next_node
+      return if prev_node.nil?
 
-   def add_at(ind, item)
-      new_node = Node.new(item)
-      if ind == 0
-        new_node.next_node = @head
-        @head = new_node
-        @tail = @head.next_node if @tail = @head
-      else
-        prev_node = earlier_node(ind)
-        cur = prev_node.next_node
-        return if prev_node.nil?
+      new_node.next_node = cur
 
-        new_node.next_node = cur
+      prev_node.next_node = new_node
+      @tail = new_node if @tail == cur
+    end
+  end
 
-        prev_node.next_node = new_node
-        @tail = new_node if (@tail == cur)
-      end
-   end
-
-   def remove(index)
-
+  def remove(index)
     if index == 0
-     @head = @head.next_node
-   else
+      @head = @head.next_node
+    else
       prev_node = earlier_node(index)
       current = prev_node.next_node
       if current
-      prev_node.next_node = prev_node.next_node.next_node
-      @tail = prev_node if @tail == current
+        prev_node.next_node = prev_node.next_node.next_node
+        @tail = prev_node if @tail == current
       end
 
     end
   end
 
+  private
 
-   private
-
-   def earlier_node(ind)
+  def earlier_node(ind)
     node = @head
     count = 0
 
-    while node && (count == (ind -2))
+    while node && (count == (ind - 2))
       node = node.next_node
-      count+=1
+      count += 1
     end
     node
-   end
+  end
 
-
-   def get_node(index)
+  def get_node(index)
     node = @head
     counter = 0
 
@@ -99,12 +96,10 @@ class LinkedList
       node = node.next_node
       counter += 1
     end
-     node
+    node
   end
-
-
 end
-  
+
 list = LinkedList.new
 
 list.add(3)
@@ -114,7 +109,6 @@ list.add_at(0, 13)
 
 puts 'list ', list.get(2)
 # => 11
-
 
 puts list.get(3)
 # => 5
---
+++ ruby/min_stack.rb
@@ -0,0 +1,61 @@
+# Start with your code from last challenge.
+
+class Node
+    attr_accessor :value, :next_node
+    
+    def initialize(value, next_node = nil)
+        @value = value
+      @next_node = next_node
+    end
+  end
+
+  
+  
+  class Stack
+    @head = nil
+	@tail = nil
+    @min  = nil
+    
+    def min
+		@min.value unless @min.nil?
+	end
+
+      def push(item)
+          new_node = Node.new(item, @head)
+          if @head.nil?
+            @tail =	new_node
+			@min = new_node
+          end
+          min_val = @min.value unless @min.nil?
+            @min = Node.new(item, @min) if min_val > item
+		    @head = new_node
+		    return
+      end
+  
+      def pop
+          raise 'empty' if @head.nil?
+          @min = @min.next_node if @min.value == @head.value
+          value = @head.value
+          @head = @head.next_node
+          value
+      end
+  end
+
+  stack = Stack.new
+stack.push(3)
+stack.push(5)
+puts stack.min
+# => 3
+
+stack.pop
+stack.push(7)
+puts stack.min
+# => 3
+
+stack.push(2)
+puts stack.min
+# => 2
+
+stack.pop
+puts stack.min
+# => 3
